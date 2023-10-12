aab358bad08402fdd569f10bb14856deff38f334
SoyGema <gema.parreno.piqueras@gmail.com>
2023-10-12 16:27:34 +0200

    change import dateutil

--- pip_rating/utils.py
+++ pip_rating/utils.py
@@ -1,11 +1,12 @@
 import sys
 from datetime import datetime
-from dateutil import parser as dateutil_parser
+
 
 def parse_iso_datetime(iso_dt):
     if sys.version_info >= (3, 11):
         dt = datetime.fromisoformat(iso_dt)
     else:
+        from dateutil import parser as dateutil_parser
         try:
             dt = datetime.fromisoformat(iso_dt)
         except ValueError:
